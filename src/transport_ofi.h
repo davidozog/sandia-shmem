cb41629d4e37778e58e5540ed05fd0d348bb581b
David Ozog <david.m.ozog@intel.com>
2017-04-24 16:21:31 -0400

    Match OFI triggered_atomic function signature
    Signed-off-by: David Ozog <david.m.ozog@intel.com>

--- src/transport_ofi.h
+++ src/transport_ofi.h
@@ -708,7 +708,7 @@ static inline
 void shmem_transport_triggered_atomic_small(const void *source, size_t len,
                                             int pe, shm_internal_op_t op, 
                                             shm_internal_datatype_t datatype,
-                                            shmem_transport_ct_t *ct, long threshold)
+                                            shmem_transport_ct_t **ct, long threshold)
 {
     RAISE_ERROR_STR("OFI transport does not currently support triggered operations");
 }
