733fea247e152c54881da304969215fd3b8217bb
David Ozog <david.m.ozog@intel.com>
2023-10-02 13:33:10 -0400

    teams: Add SHMEMX_TEAM_HOSTS pre-defined team

--- test/shmemx/shmemx_team_host.c
+++ test/shmemx/shmemx_team_host.c
@@ -1,28 +1,9 @@
-/*
- *  Copyright (c) 2019 Intel Corporation. All rights reserved.
- *  This software is available to you under the BSD license below:
+/* Copyright (c) 2023 Intel Corporation.
+ * SPDX-License-Identifier: BSD-3-Clause
  *
- *      Redistribution and use in source and binary forms, with or
- *      without modification, are permitted provided that the following
- *      conditions are met:
- *
- *      - Redistributions of source code must retain the above
- *        copyright notice, this list of conditions and the following
- *        disclaimer.
- *
- *      - Redistributions in binary form must reproduce the above
- *        copyright notice, this list of conditions and the following
- *        disclaimer in the documentation and/or other materials
- *        provided with the distribution.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * This file is part of the Sandia OpenSHMEM software package. For license
+ * information, see the LICENSE file in the top level directory of the
+ * distribution.
  */
 
 #include <stdio.h>
@@ -51,7 +32,7 @@ int main(void)
     printf("[PE: %d] SHMEM_TEAM_HOST peers: { ", me);
     for (int i = 0; i < npes; i++) {
         if (shmem_team_translate_pe(SHMEM_TEAM_WORLD, i,
-                                     SHMEMX_TEAM_HOST) != -1) {
+                                    SHMEMX_TEAM_HOST) != -1) {
             peers[num_peers++] = i;
             printf("%d ", i);
         }
@@ -71,4 +52,3 @@ int main(void)
     shmem_finalize();
     return 0;
 }
-  
